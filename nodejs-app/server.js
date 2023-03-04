eac54ab45d76f4ac9f6ff6cb37d0b671347f84df
Student User <student@workstation.lab.example.com>
2023-03-04 16:50:31 -0500

    adding new line

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send("Hello World\n");
 });
 
 app.listen(8080, function () {
